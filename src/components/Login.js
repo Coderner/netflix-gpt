9b694b266fb8a005c8523cc2c0838e5613a5f4cb
coderner <shreyashukla680@gmail.com>
2024-01-07 18:35:16 +0530

    added validations to forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
 import BackgroundImage from '../images/background.jpg'
+import { checkValidateData } from '../utils/validate'
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const fullName = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const [validateResponse,setValidateResponse] = useState({});
 
   const toggleSignInForm = () =>{
     setIsSignInForm(!isSignInForm);
+    {!isSignInForm && (fullName.current.value="")};
+    email.current.value="";
+    password.current.value="";
+    setValidateResponse({});
+  }
+
+  const handleButtonClick = () =>{
+     if(isSignInForm){
+        setValidateResponse(checkValidateData(undefined,email.current.value,password.current.value));
+     }
+     else{
+        setValidateResponse(checkValidateData(fullName.current.value,email.current.value,password.current.value));
+     }
+     console.log(validateResponse);
   }
 
   return (
@@ -15,28 +34,39 @@ const Login = () => {
         <div className='absolute'>
            <img src={BackgroundImage} alt="background"/>
         </div>
-        <form className="relative mx-auto top-36 px-20 py-16 w-2/5 flex flex-col bg-black/90 rounded-md">
+        <form onSubmit={(e)=> e.preventDefault()} className="relative mx-auto top-36 px-20 py-16 w-2/5 flex flex-col bg-black/90 rounded-md">
             <h1 className='text-white text-3xl font-semibold mb-5'>
                 {isSignInForm? "Sign In" : "Sign Up"}
             </h1>
             {!isSignInForm && (
-              <input 
-                type="text" 
-                placeholder="Full Name" 
-                className="px-4 py-2 my-2 rounded-md bg-[#333333] text-white"
-              />
+              <>
+                <input 
+                  ref={fullName}
+                  type="text" 
+                  placeholder="Full Name" 
+                  className="px-4 py-2 my-2 rounded-md bg-[#333333] text-white"
+                />
+                <p className="text-sm font-medium text-[#cf0e07]">{validateResponse.fullNameError}</p>
+              </>
             )}
             <input 
+              ref={email}
               type="text" 
               placeholder="Email Address" 
               className="px-4 py-2 my-2 rounded-md bg-[#333333] text-white"
             />
+             <p className="text-sm font-medium text-[#cf0e07]">{validateResponse.emailError}</p>
             <input 
+              ref={password}
               type="password" 
               placeholder="Password" 
               className="px-4 py-2 my-2 rounded-md bg-[#333333] text-white"
             />
-            <button className="p-2 mt-7 mb-5 bg-[#cf0e07] text-white px-4 py-2 rounded-md">
+             <p className="text-sm font-medium text-[#cf0e07]">{validateResponse.passwordError}</p>
+            <button 
+              className="p-2 mt-7 mb-5 bg-[#cf0e07] text-white px-4 py-2 rounded-md"
+              onClick={handleButtonClick}
+            >
                 {isSignInForm? "Sign In" :"Sign Up"}
             </button>
             <h1 className='text-white hover:cursor-pointer' onClick={()=>toggleSignInForm()}>
---
+++ src/utils/validate.js
@@ -0,0 +1,24 @@
+export const checkValidateData = (fullName,email,password) =>{
+  const error = {
+    fullNameError:"",
+    emailError:"", 
+    passwordError:""
+  }
+  
+  if(!fullName)
+     error.fullNameError = "Name can't be empty";
+  else if(fullName.length<4)
+     error.fullNameError = "Name is too short must be atleast 4 characters";
+
+  if(!email)
+     error.emailError = "Email can't be empty";
+  else if(!(/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email)))
+     error.emailError = "Invalid Email";
+
+  if(!password)
+     error.passwordError = "Password can't be empty";
+  else if(password.length<8)
+     error.passwordError = "Password must be atleast 8 charcters long";
+  
+  return error;
+}
