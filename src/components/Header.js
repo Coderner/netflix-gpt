ad50da8ca723f6c230b96723a13e3e19c2635509
coderner <shreyashukla680@gmail.com>
2024-01-07 13:12:17 +0530

    Created a SignIn page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Logo from "../images/logo.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='absolute px-20 py-8 bg-gradient-to-b from-black z-10'>
+        <img src={Logo} alt="logo" className='w-1/5'/>
+    </div>
   )
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import Header from './Header'
+import BackgroundImage from '../images/background.jpg'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+        <Header/>
+        <div className='absolute'>
+           <img src={BackgroundImage} alt="background"/>
+        </div>
+        <form className="relative mx-auto top-36 px-20 py-16 w-2/5 flex flex-col bg-black/90 rounded-md">
+            <h1 className='text-white text-3xl font-semibold mb-5'>Sign In</h1>
+            <input 
+              type="text" 
+              placeholder="Email Address" 
+              className="px-4 py-2 my-2 rounded-md bg-[#333333] text-white"
+            />
+            <input 
+              type="password" 
+              placeholder="Password" 
+              className="px-4 py-2 my-2 rounded-md bg-[#333333] text-white"
+            />
+            <button className="p-2 mt-7 mb-5 bg-[#cf0e07] text-white px-4 py-2 rounded-md">Sign In</button>
+            <h1 className='text-white'>New to NetflixGPT? Sign up Now</h1>
+        </form>
+    </div>
   )
 }
 
